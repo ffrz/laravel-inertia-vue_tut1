b9ca4e42c7ed2fffa67e6e670fb9d8ee9321b859
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-09-24 13:34:14 +0700

    finished part 11

---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+const model = defineModel({
+  type: null,
+  required: true,
+})
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  error: String,
+})
+</script>
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input :type="type" v-model="model" :class="{ '!ring-red-500': error }" />
+    <p class="error" v-if="error">{{ error }}</p>
+  </div>
+</template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3';
+import TextInput from '../../Components/TextInput.vue';
 
 const form = useForm({
   name: null,
@@ -16,29 +17,16 @@ const submit = () => {
 </script>
 
 <template>
+
   <Head title="Register" />
   <h1 class="title">Register a new account</h1>
   <div class="w-2/4 mx-auto">
     <form class="space-y-6" @submit.prevent="submit">
-      <div>
-        <label>Name</label>
-        <input type="text" v-model="form.name"/>
-        <p class="error" v-if="form.errors.name">{{ form.errors.name }}</p>
-      </div>
-      <div>
-        <label>Email</label>
-        <input type="email" v-model="form.email" />
-        <p class="error" v-if="form.errors.email">{{ form.errors.email }}</p>
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" v-model="form.password" />
-        <p class="error" v-if="form.errors.password">{{ form.errors.password }}</p>
-      </div>
-      <div>
-        <label>Password Confirmation</label>
-        <input type="password" v-model="form.password_confirmation" />
-      </div>
+      <TextInput label="Name" type="text" v-model="form.name" :error="form.errors.name" />
+      <TextInput label="Email" type="email" v-model="form.email" :error="form.errors.email" />
+      <TextInput label="Password" type="password" v-model="form.password" :error="form.errors.password" />
+      <TextInput label="Password Confirmation" type="password" v-model="form.password_confirmation"
+        :error="form.errors.password_confirmation" />
       <div>
         <p class="text-slate-600">Do not have an account? <a href="#" class="text-link">Register</a></p>
       </div>
