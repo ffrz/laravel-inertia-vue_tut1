7dd7d0ebb058bff65454d522e45459c022679813
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-09-24 18:29:47 +0700

    finished part 17

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,11 +1,18 @@
 <script setup>
-
+import { watch } from 'vue';
+import { router } from '@inertiajs/vue3';
+import { ref } from 'vue';
 import PaginationLinks from '../Components/PaginationLinks.vue';
+import { debounce } from 'lodash';
 
-defineProps({
+const props = defineProps({
   users: {
     type: Object,
     required: true,
+  },
+  search: {
+    type: String,
+    default: '',
   }
 });
 
@@ -17,12 +24,20 @@ const formatDate = (date) => {
   });
 }
 
+const search = ref(props.search);
+
+watch(search, debounce((q) => router.get('/', { search: q }, { preserveState: true }), 500));
 
 </script>
 
 <template>
 
   <Head title="Users" />
+  <div class="flex justify-end mb-4">
+    <div class="w-1/4">
+      <input type="search" placeholder="Search" v-model="search">
+    </div>
+  </div>
   <table>
     <thead>
       <tr>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,22 @@
 
 use App\Http\Controllers\AuthController;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::inertia('/', 'Index', ['users' => User::paginate(5)])->name('home');
+Route::get('/', function (Request $request) {
+    return inertia('Index', [
+        'users' => User::when(
+            $request->search,
+            function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->search . '%')
+                    ->orWhere('email', 'like', '%' . $request->search . '%');
+            }
+        )->paginate(5)->withQueryString(),
+        'search' => $request->search
+    ]);
+})->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
